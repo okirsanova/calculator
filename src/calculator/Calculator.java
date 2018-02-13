da583be1754eae033089eb2aca8ed13ab9650ee8
okirsanova <aspirjanda@gmail.com>
2018-02-13 20:28:49 +0200

    calulator option for "+"; "#; "-"; "/"; "*".

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,7 +8,33 @@ package calculator;
 public class Calculator {
 
     public String calculate(String[] expression) {
-        return "0";
-    }
 
+        String a = expression[0];
+        String b = expression[2];
+        String op = expression[1];
+
+        double da = Double.parseDouble(a);
+        double db = Double.parseDouble(b);
+
+        double result = 0; // not needed, can be used instead of default
+        switch (op) {
+            case "+":
+            case "#":
+                result = da + db;
+                break;
+            case "-":
+                result = da - db;
+                break;
+            case "*":
+                result = da * db;
+                break;
+            case "/":
+            result = da / db;
+            break;
+            default:
+                return "Error";
+                }
+
+        return String.valueOf(result);
+    }
 }
